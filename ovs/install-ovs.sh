10142ea9e87e77094711178ae55b28850f705e51
JongKI-PARK <97037853+JongKI-PARK@users.noreply.github.com>
2023-08-13 07:41:25 +0900

    Update install-ovs.sh

--- ovs/install-ovs.sh
+++ ovs/install-ovs.sh
@@ -5,6 +5,7 @@ sudo apt install jq -y
 sudo apt install bridge-utils -y
 sudo apt install net-tools -y
 sudo apt install traceroute -y
+sudo mkdir -p /etc/cni/net.d/
 
 # Install OVS
 sudo apt-get update
